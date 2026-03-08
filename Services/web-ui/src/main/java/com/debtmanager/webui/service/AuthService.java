7c72ae00f384beaad8471d91d482043f47926a94
RONALD ALBERTO REYES SANCHEZ <rr2024-1087@uce.edu.do>
2026-03-08 17:28:00 -0400

    Implementacion de capa visual del dashboard con sus respectivos controllers, services y dtos

---
+++ Services/web-ui/src/main/java/com/debtmanager/webui/service/AuthService.java
@@ -0,0 +1,18 @@
+package com.debtmanager.webui.service;
+
+import com.debtmanager.webui.client.AuthClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final AuthClient authClient;
+
+    public AuthService(AuthClient authClient) {
+        this.authClient = authClient;
+    }
+
+    public String login(String email, String password) {
+        return authClient.login(email, password);
+    }
+}
